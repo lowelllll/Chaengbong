c01a12e37db3b003008a497ffacba31ec8eb3bca
lowell <yejin2651@naver.com>
2018-12-06 18:58:58 +0900

    날씨 기능 수정
    api가 별루라서 crawler로 대체

--- plusfriend/module/wether.py
+++ plusfriend/module/wether.py
@@ -1,9 +1,11 @@
 import datetime
 import requests
 from xml.etree import ElementTree
+from bs4 import BeautifulSoup
 
 class Weather():
-    weather = []
+    weathers = [] # 예보
+    now_weather = {} # 현재 날씨
 
     @staticmethod
     def _xml_parse():
@@ -22,23 +24,38 @@ class Weather():
         Weather.weather.sort(key=lambda x:x['hour'])
 
     @staticmethod
-    def _calc_time():
-        now_hour = datetime.datetime.now().hour
-        hour = now_hour if now_hour % 3 == 0 else now_hour+(3-(now_hour%3))
-        return hour
+    def _crawler():
+        res = requests.get('http://www.weather.go.kr/weather/process/timeseries-dfs-body-ajax.jsp?myPointCode=1162069500&unit=M')
+        soup = BeautifulSoup(res.text, 'html.parser')
+        data = soup.select(
+            'dl > dd.now_weather1_center'
+        )
+        print(data)
+
+        Weather.now_weather['hour'] = datetime.datetime.now().hour
+        Weather.now_weather['temp'] = data[0].text
+        # now['humidity'] = data[2].text 습도
+
+        datas = soup.find_all('dl','time_weather1_dl3')
+        for data in datas:
+            w = {}
+            w['hour'] = data.find('dt','w_hour2').text
+            w['comment'] = data.find('img').get('alt')
+            w['temp'] = data.find('dd','temp2').text
+            Weather.weathers.append(w)
 
     @staticmethod
-    def _create_message(hour):
-        idx = hour // 3 if hour != 3 else 0
-        w_data = Weather.weather[idx]
-
-        msg = """관악구 신림동 실시간 날씨 예보\n{hour}시 기준\n최고 기온은 {tmn} 이고, 최저 기온은 {tmx} 입니다.\n날씨는 {comment}입니다.""".\
-            format(hour=hour, tmn=w_data['tmn'], tmx=w_data['tmx'], comment=w_data['comment'])
+    def _create_message():
+        msg = '관악구 신림동 현재 {}시\n기온은 {} 입니다.\n'.format(Weather.now_weather['hour'], Weather.now_weather['temp'])
+        msg += '날씨 예보!\n'
+        for weather in Weather.weathers:
+            msg += '{}입니다. \n예상 기온 {}, 날씨 {}입니다.\n'.format(weather['hour'], weather['temp'], weather['comment'])
 
         return msg
 
     @staticmethod
     def process():
-        Weather._xml_parse()
-        hour = Weather._calc_time()
-        return Weather._create_message(hour)
+        Weather._crawler()
+        print(Weather._create_message())
+
+Weather.process()
